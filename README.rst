c77794ec868a48c62b44a877f66db34dea6c402f
Wolfgang Richter <wolf@cs.cmu.edu>
2012-06-23 18:17:39 -0400

    movi: initial commit

---
+++ README.rst
@@ -0,0 +1,21 @@
+[TOC -0] Table of Contents
+-------------------------------------------------------------------------------
+
+0) TOC -0   Table of Contents
+1) INTR-1   Introduction
+
+
+
+
+[INTR-1] Introduction
+-------------------------------------------------------------------------------
+Simple synchronized movie streamer.
+
+Uses Flowplayer and Flask.
+
+
+
+
+[LICE-2] License
+-------------------------------------------------------------------------------
+Released open source under the MIT License. See the included LICENSE file.
---
+++ src/movi.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+#
+#   Authors: Wolfgang Richter <wolf@cs.cmu.edu>
+#            Debjani Biswas   <dbiswas@cmu.edu>
+#
+#
+#   Description: This is a web app that serves a group of video files from a
+#                directory.
+
+
+### Imports ###
+from flask import Flask
+
+
+from sys import argv
+import os 
+
+
+
+
+### App Configuration ###
+USAGE='Usage: %s <path to movie files folder>'
+HOST='0.0.0.0'
+PORT=31415
+app=Flask(__name__)
+app.debug=True
+
+__movies = dict()
+
+### Helper functions ###
+def die(msg):
+    print 'ERROR: %s' % (msg)
+    exit(1)
+
+def find_movies(path):
+    global __movies
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        for d in dirnames:
+            find_movies(d)
+        for f in filenames:
+            split = os.path.splitext(f)
+            if split[1] == '.avi':
+                __movies[split[0]] = os.path.join(dirpath, f)
+
+
+
+
+### Routes ###
+@app.route('/')
+def index():
+    return 'Welcome to the movie player.'
+
+@app.route('/info')
+def info():
+    global __movies
+    sorted_movies = sorted(__movies.items())
+    return 'Display configuration and list of videos (%s)' % (str(sorted_movies))
+
+@app.route('/serve/<name>')
+def serve(name=None):
+    return 'Trying to serve: "%s"' % name
+
+
+@app.route('/watch/<name>')
+def serve(name=None)
+    return 'Trying to watch: "%s"' % name
+
+### Main ###
+if __name__ == '__main__':
+    
+    if len(argv) != 2:
+        print USAGE % (argv[0])
+        die('Not enough arguments.')
+
+    if not os.path.isdir(argv[1]):
+        print USAGE % (argv[1])
+        die('First argument is not a valid directory.')
+
+  
+    find_movies(argv[1])
+
+    app.run(host=HOST, port=PORT)
